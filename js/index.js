1042d176b7bf90309e312b2319badba514b1c615
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-06-07 18:54:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+  if (window.location.pathname == '/' || window.location.pathname.endsWith('GustoPorLoBueno')) { window.location.replace(window.location.origin + window.location.pathname + 'index.html')}
+  
+  
   const currentPath = window.location.pathname.split('/').filter(seg => seg).pop() || 'index.html';
   document.querySelectorAll('.menu li a').forEach(link => {
     const linkPath = link.getAttribute('href').split('/').filter(seg => seg).pop();
